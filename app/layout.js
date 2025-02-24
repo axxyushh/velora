52dbe1f94d3827f1e555d8689cbca4286040923c
ayush <flamedeveloper2021@gmail.com>
2025-02-24 19:26:08 +0530

    Project Failed Successfully.

--- app/layout.js
+++ app/layout.js
@@ -1,32 +1,29 @@
 import Header from "@/components/header";
 import "./globals.css";
-import { Inter } from "next/font/google"
+import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
-import Footer from "@/components/footer";
 import { Toaster } from "sonner";
+import "react-quill-new/dist/quill.snow.css";
 
-const inter = Inter({subsets:["latin"]});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Velora",
-  description: "Your journaling method.",
+  title: "Reflct",
+  description: "",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+    >
       <html lang="en">
         <body
-          className={`${inter.className}`}
+          className={`${inter.className} bg-gradient-to-b from-green-50 via-amber-50 to-green-50`}
         >
           <div className="inset-0 bg-[url('/bg.jpg')] opacity-50 fixed -z-10" />
-
-          <Header/>
-
-          <main className="min-h-screen">
-            {children}
-          </main>
-          <Toaster richColors/>
+          <Header />
+          <main className="min-h-screen">{children}</main>
+          <Toaster richColors />
 
           <Footer/>
           
